ca3872751e7e1e8ce560a4a50f9bec52549d19c2
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-11-20 04:06:25 +0900

    fix

--- scripts/depends.sh
+++ scripts/depends.sh
@@ -4,7 +4,7 @@ set -e
 
 # DEPENDS="readline zlib libffi bzip2 xz lcov ncurses sqlite ssl tcl tk wasix"
 # TOOLS="make gdb gdbm cmake ninja wasi-sdk-prebuilt emsdk"
-TOOLS="wasix"
+TOOLS="wasi-sdk-prebuilt wasix"
 DEPENDS_SCRIPT="$(dirname ${0})/depends"
 
 export PREFIX="${HOME}/.local"
@@ -16,6 +16,6 @@ DEPENDS="${TOOLS} ${DEPENDS}"
 for DEPEND in ${DEPENDS}; do
   echo "installing ${DEPEND} ..."
   DEFAULT_PWD="${PWD}"
-  . "${DEPENDS_SCRIPT}/install_${DEPEND}.sh"
+  "${DEPENDS_SCRIPT}/install_${DEPEND}.sh"
   cd "${DEFAULT_PWD}"
 done
