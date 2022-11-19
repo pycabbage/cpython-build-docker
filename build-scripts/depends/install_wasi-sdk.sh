2d58eaaeab29c20bff9754c8cfac0389f46ddad7
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-11-20 03:07:28 +0900

    fix?

--- build-scripts/depends/install_wasi-sdk.sh
+++ build-scripts/depends/install_wasi-sdk.sh
@@ -7,10 +7,10 @@ VERSION="wasi-sdk-16"
 DIRNAME="${NAME}"
 REPO="https://github.com/WebAssembly/wasi-sdk.git"
 
-git clone --depth 1 --single-branch -b "${VERSION}" "${REPO}" "${DIRNAME}"
+# git clone --depth 1 --single-branch -b "${VERSION}" "${REPO}" "${DIRNAME}"
 cd "${DIRNAME}"
-git submodule update --init --recursive --recommend-shallow --depth 1
-make package -j8
+# git submodule update --init --recursive --recommend-shallow --depth 1
+NINJA_FLAGS=-v make package -j8
 ls dist
 make install PREFIX="${PREFIX}"
 cd -
