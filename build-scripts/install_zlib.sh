9738fb8826a0fb99d77e2b21faff802857d0cf20
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-11-19 19:33:44 +0900

    fix

--- build-scripts/install_zlib.sh
+++ build-scripts/install_zlib.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-VERSION=1.2.13
+VERSION="1.2.13"
 DIRNAME="zlib-${VERSION}"
-FILENAME="${DIRNAME}.tar.xz"
+FILENAME="${DIRNAME}.tar.gz"
 URL="https://zlib.net/fossils/${FILENAME}"
 
 curl "${URL}" -o "${FILENAME}"
-tar Jxf "${FILENAME}"
+tar zxf "${FILENAME}"
 cd "${DIRNAME}"
 make -j8
 make install
